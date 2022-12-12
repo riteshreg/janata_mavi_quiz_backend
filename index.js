7eff85dce92ec9b279d5172aa117849b55d47c30
Ritesh Regmi <71575903+riteshreg@users.noreply.github.com>
2022-12-12 04:49:12 +0000

    delete api

--- index.js
+++ index.js
@@ -10,39 +10,47 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
   let data = await quizModel.find({})
   res.send(data)
 })
 
 
-app.get('/random_question', (req, res)=>{
-  quizModel.findRandom({}, {}, {limit: 10}, function(err, results) {
+app.get('/random_question', (req, res) => {
+  quizModel.findRandom({}, {}, { limit: 10 }, function(err, results) {
     if (!err) {
-      res.send(results) 
+      res.send(results)
     }
   });
 })
 
-app.post('/login',async(req,res)=>{
 
-  if(req.body.userName && req.body.password){
+app.delete('/delete', (req, res) => {
+  quizModel.deleteOne({ _id: req.body._id }).then((response) => {
+    res.send(response)
+  })
+
+})
+
+app.post('/login', async (req, res) => {
+
+  if (req.body.userName && req.body.password) {
     let user = await userModel.findOne(req.body).select("-password")
-      if(user){
-        res.send(user)
-      }else{
-        res.send({err:"no user found"})
-      }
-  
-  }else{
-    res.send({err:"please enter both userName and password"})
+    if (user) {
+      res.send(user)
+    } else {
+      res.send({ err: "no user found" })
+    }
+
+  } else {
+    res.send({ err: "please enter both userName and password" })
   }
 
 })
 
-app.post('/', async(req, res)=>{
+app.post('/', async (req, res) => {
   let data = new quizModel(req.body)
-  data =  await data.save();
+  data = await data.save();
   res.send(data)
 
 })
